4eef122777a3ee0ceec489606ff1b73b5c9dc704
Binuprasad <binuprasad2000@gmail.com>
2023-05-07 11:47:25 +0530

    ui completed

---
+++ lib/view/detail_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weather_app/consts/appconstants.dart';
+
+import '../widgets/graph_container_widget.dart';
+import '../widgets/weather_details_container_widget.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {
+              Get.back();
+            },
+            icon: const Icon(
+              Icons.chevron_left_sharp,
+              color: Colors.black,
+              size: 30,
+            )),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {},
+                child: Row(
+                  children: const [Text('Added to list'), Icon(Icons.done)],
+                )),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppConstants.height30,
+            Image.asset('assets/sunny.png'),
+            AppConstants.height10,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Pune',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                ImageIcon(AssetImage('assets/navigation.png'))
+              ],
+            ),
+            AppConstants.height10,
+            const Text(
+              '20°',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            AppConstants.height10,
+            AppConstants.height10,
+            const WeatherDetailsContainerWidget(),
+            AppConstants.height10,
+            const GraphContainerWidget()
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/view/search_result.dart
+++ lib/view/search_result.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:weather_app/consts/appconstants.dart';
+import 'package:weather_app/view/detail_page.dart';
 
 class SearchResult extends StatelessWidget {
   const SearchResult({super.key});
@@ -34,57 +36,60 @@ class SearchResult extends StatelessWidget {
           ],
         ),
         AppConstants.height20,
-        Container(
-          height: AppConstants.height(context) * 0.3,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.grey.shade200),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text(
-                      'Mumbai',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    ),
-                    const Text(
-                      '20°',
-                      style:
-                          TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.warning,
-                          color: Colors.amber,
-                        ),
-                        SizedBox(
-                          width: AppConstants.width(context) * 0.03,
-                        ),
-                        const Text(
-                          'WARNING',
-                          style: TextStyle(color: Colors.amber,fontSize: 16),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Image.asset('assets/cloudy.png'),
-                    const Text(
-                      'Expecting Rainfall',
-                      style: TextStyle(color: Colors.amber, fontSize: 16),
-                    )
-                  ],
-                )
-              ],
+        GestureDetector(
+          onTap: () => Get.to(const DetailPage()),
+          child: Container(
+            height: AppConstants.height(context) * 0.3,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.grey.shade200),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Mumbai',
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                      const Text(
+                        '20°',
+                        style:
+                            TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.warning,
+                            color: Colors.amber,
+                          ),
+                          SizedBox(
+                            width: AppConstants.width(context) * 0.03,
+                          ),
+                          const Text(
+                            'WARNING',
+                            style: TextStyle(color: Colors.amber,fontSize: 16),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset('assets/cloudy.png'),
+                      const Text(
+                        'Expecting Rainfall',
+                        style: TextStyle(color: Colors.amber, fontSize: 16),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         )
